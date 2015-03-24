47a091afa0ea43d445ecafe90d534c85f5da46d7
josenaldo <josenaldo@gmail.com>
2015-03-24 19:45:34 -0300

    Google Analytics

--- js/presentation.js
+++ js/presentation.js
@@ -7,7 +7,7 @@ Reveal.initialize({
 	center: true,
 	slideNumber: true,
 	theme: Reveal.getQueryHash().theme, // available themes are in /css/theme
-	transition: Reveal.getQueryHash().transition || 'slide', // default/cube/page/concave/zoom/linear/fade/none
+	transition: Reveal.getQueryHash().transition || 'slide', // none/fade/slide/convex/concave/zoom
 	// Parallax scrolling
 	// parallaxBackgroundImage: 'https://s3.amazonaws.com/hakim-static/reveal-js/reveal-parallax-1.jpg',
 	// parallaxBackgroundSize: '2100px 900px',
@@ -16,9 +16,9 @@ Reveal.initialize({
 		{ src: 'lib/js/classList.js', condition: function() { return !document.body.classList; } },
 		{ src: 'plugin/markdown/marked.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
 		{ src: 'plugin/markdown/markdown.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
-		{ src: 'plugin/highlight/highlight.js', async: true, callback: function() { hljs.initHighlightingOnLoad(); } },
-		{ src: 'plugin/zoom-js/zoom.js', async: true, condition: function() { return !!document.body.classList; } },
-		{ src: 'plugin/notes/notes.js', async: true, condition: function() { return !!document.body.classList; } }
+		{ src: 'plugin/highlight/highlight.js', async: true, condition: function() { return !!document.querySelector( 'pre code' ); }, callback: function() { hljs.initHighlightingOnLoad(); } },
+		{ src: 'plugin/zoom-js/zoom.js', async: true },
+		{ src: 'plugin/notes/notes.js', async: true }
 	]
 });
 
